9ae8f7d178f0199b185117a64c21808049ce6caa
cbb356 <cbb356@gmail.com>
2025-08-22 02:24:07 +0300

    001_HW template added

---
+++ 001_User_Collections/001_User_Collections_HW/001_User_Collections_HW/06_EvenPower/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Створіть метод, який як аргумент приймає масив цілих чисел і 
+ * повертає колекцію квадратів усіх непарних чисел масиву. 
+ * Для формування колекції використовуйте оператор yield.
+ */
+
+namespace EvenPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
