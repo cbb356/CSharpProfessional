1d6221164cf118ba60b04f5f260aab8047b84824
cbb356 <cbb356@gmail.com>
2025-09-06 01:24:23 +0300

    003_04_HW added

--- 003_Input_Output/003_Input_Output_HW/04_Sandbox/MainWindow.xaml.cs
+++ 003_Input_Output/003_Input_Output_HW/04_Sandbox/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.IO.IsolatedStorage;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,68 @@ namespace Sandbox
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string fileName = "test.txt";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Creating the isolated storage
+            using (IsolatedStorageFile isolatedStore = IsolatedStorageFile.GetUserStoreForAssembly())
+            {
+                try
+                {
+                    // Creating a stream to write to a file in the isolated store
+                    using (var stream = new IsolatedStorageFileStream(fileName, FileMode.Create, isolatedStore))
+                    {
+                        using (var writer = new StreamWriter(stream))
+                        {
+                            // Writing the content of the TextBox to the file
+                            writer.Write(DataTextBox.Text);
+                        }
+                    }
+                    StatusTextBlock.Text = "Data saved successfully";
+                    DataTextBox.Clear();
+                }
+                catch (IOException ex)
+                {
+                    StatusTextBlock.Text = $"Error saving data: {ex.Message}";
+                }
+            }
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (IsolatedStorageFile isolatedStore = IsolatedStorageFile.GetUserStoreForAssembly())
+            {
+                try
+                {
+                    // Checking if the file exists before trying to read it
+                    if (isolatedStore.FileExists(fileName))
+                    {
+                        // Creating a stream to read from the file
+                        using (var stream = new IsolatedStorageFileStream(fileName, FileMode.Open, isolatedStore))
+                        {
+                            using (var reader = new StreamReader(stream))
+                            {
+                                // Reading the content and update the TextBox
+                                DataTextBox.Text = reader.ReadToEnd();
+                            }
+                        }
+                        StatusTextBlock.Text = "Data loaded successfully";
+                    }
+                    else
+                    {
+                        StatusTextBlock.Text = "No saved data found";
+                    }
+                }
+                catch (IOException ex)
+                {
+                    StatusTextBlock.Text = $"Error loading data: {ex.Message}";
+                }
+            }
         }
     }
 }
