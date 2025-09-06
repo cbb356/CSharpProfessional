d2df1a0761fbc16a38131c4fa98cfa5f407fadd3
cbb356 <cbb356@gmail.com>
2025-09-06 23:09:57 +0300

    004_06_HW refactored

--- 004_Text/004_Text_HW/06_Registration/Program.cs
+++ 004_Text/004_Text_HW/06_Registration/Program.cs
@@ -11,28 +11,37 @@ namespace Registration
     {
         static void Main(string[] args)
         {
-            string? login, password, input = string.Empty;
-            string loginPattern = @"^[a-zA-Z]+$";
-            string passwordPattern = @"^[a-zA-Z0-9]+$";
-            Regex regexLogin = new Regex(loginPattern);
-            Regex regexPassword = new Regex(passwordPattern);
+            string? login, password;
 
-            while (!regexLogin.IsMatch(input ?? string.Empty))
+            while (true)
             {
-                Console.WriteLine("Create the login (only latin letters):");
-                input = Console.ReadLine();
+                Console.WriteLine("Enter login (Latin alphabet characters only):");
+                login = Console.ReadLine()?.Trim() ?? string.Empty;
+                if (Regex.IsMatch(login, @"^[a-zA-Z]+$"))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid login. Use only Latin letters");
+                }
             }
-            login = input;
 
-            input = String.Empty;
-            while (!regexPassword.IsMatch(input ?? string.Empty))
+            while (true)
             {
-                Console.WriteLine("Create the password (only latin letters and numbers):");
-                input = Console.ReadLine();
+                Console.WriteLine("Enter password (Latin alphabet characters and digits only):");
+                password = Console.ReadLine()?.Trim() ?? string.Empty;
+                if (Regex.IsMatch(password, @"^[a-zA-Z0-9]+$"))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid password. Use only Latin letters and digits");
+                }
             }
-            password = input;
 
-            Console.WriteLine($"Login: {login}");
+            Console.WriteLine($"\nLogin: {login}");
             Console.WriteLine($"Password: {password}");
 
             // Delay
