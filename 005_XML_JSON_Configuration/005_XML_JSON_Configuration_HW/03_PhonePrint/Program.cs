6e793eb35ff6da823f6bd83a8ea869e0aaa2ee3d
cbb356 <cbb356@gmail.com>
2025-09-21 15:57:27 +0300

    005_03_HW added

--- 005_XML_JSON_Configuration/005_XML_JSON_Configuration_HW/03_PhonePrint/Program.cs
+++ 005_XML_JSON_Configuration/005_XML_JSON_Configuration_HW/03_PhonePrint/Program.cs
@@ -2,6 +2,8 @@
  * З файлу TelephoneBook.xml (файл повинен був бути створений у процесі виконання 
  * додаткового завдання) виведіть на екран лише номери телефонів.
  */
+using System.Reflection.PortableExecutable;
+using System.Xml;
 
 namespace PhonePrint
 {
@@ -9,6 +11,21 @@ namespace PhonePrint
     {
         static void Main(string[] args)
         {
+            using (var xmlReader = new XmlTextReader("TelephoneBook.xml"))
+            {
+                Console.WriteLine("List of phone numbers:");
+                while (xmlReader.Read())
+                {
+                    if (xmlReader.NodeType == XmlNodeType.Element)
+                    {
+                        if (xmlReader.Name.Equals("Contact"))
+                        {
+                            Console.WriteLine(xmlReader.GetAttribute("TelephoneNumber"));
+                        }
+                    }
+                }
+            }
+
             // Delay
             Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey();
