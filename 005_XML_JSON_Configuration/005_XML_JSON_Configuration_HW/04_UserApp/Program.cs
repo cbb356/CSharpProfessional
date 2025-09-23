b6fe642977399ffbec0170bfe33ea6239d774cdc
cbb356 <cbb356@gmail.com>
2025-09-23 08:48:17 +0300

    005_04_HW refactored

--- 005_XML_JSON_Configuration/005_XML_JSON_Configuration_HW/04_UserApp/Program.cs
+++ 005_XML_JSON_Configuration/005_XML_JSON_Configuration_HW/04_UserApp/Program.cs
@@ -12,8 +12,7 @@ namespace UserApp
     {
         static void Main(string[] args)
         {
-            var fgColor = ConsoleColor.Gray;
-            var bgColor = ConsoleColor.Black;
+            ConsoleColor fgColor, bgColor;
             
             string fileName = "config.json";
             string filePath = Path.Combine(Path.GetTempPath(), fileName);
@@ -25,17 +24,15 @@ namespace UserApp
                     .AddJsonFile(filePath, optional: false, reloadOnChange: true)
                     .Build();
 
-                string ?fgColorString = config["ForegroundColor"];
-                string ?bgColorString = config["BackgroundColor"];
                 Console.WriteLine("The config file was read");
 
-                if (!Enum.TryParse(fgColorString, ignoreCase: true, out fgColor))
+                if (!Enum.TryParse(config["ForegroundColor"], ignoreCase: true, out fgColor))
                 {
                     Console.WriteLine("The foreground color in config is incorrect. Default value (Gray) was used instead");
                     fgColor = ConsoleColor.Gray;
                 }
 
-                if (!Enum.TryParse(bgColorString, ignoreCase: true, out bgColor))
+                if (!Enum.TryParse(config["BackgroundColor"], ignoreCase: true, out bgColor))
                 {
                     Console.WriteLine("The background color in config is incorrect. Default value (Black) was used instead");
                     bgColor = ConsoleColor.Black;
@@ -44,12 +41,17 @@ namespace UserApp
             else
             {
                 Console.WriteLine("The config file is not found. Default values (Gray and Black) were used instead");
+                fgColor = ConsoleColor.Gray;
+                bgColor = ConsoleColor.Black;
             }
 
-            Console.ForegroundColor = fgColor;
-            Console.BackgroundColor = bgColor;
             Console.WriteLine($"The foreground color set to {fgColor.ToString()}");
             Console.WriteLine($"The background color set to {bgColor.ToString()}");
+            Console.ForegroundColor = fgColor;
+            Console.BackgroundColor = bgColor;
+
+            // Testing output
+            Console.WriteLine("\nTest text");
 
             Console.ResetColor();
 
