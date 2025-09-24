d43ae628b0f74ff4b66d143c575d5b7fd59feedd
cbb356 <cbb356@gmail.com>
2025-09-25 01:32:35 +0300

    006_02_03_HW added

---
+++ 006_Reflection/006_Reflection_HW/02_TemperatureConverterLibrary/Temperature.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Створіть власну збірку на основі прикладу збірки CarLibrary  з уроку,  збірка буде використана для
+ * роботи з перетворювачем температури. 
+ */
+
+namespace TemperatureConverterLibrary
+{
+    public class Temperature
+    {
+        private decimal _temperature;
+
+        public decimal Celsius
+        {
+            get 
+            { 
+                return _temperature; 
+            }
+            set
+            {
+                if (value < -273.15m)
+                {
+                    throw new ArgumentOutOfRangeException($"{value} is less than absolute zero.");
+                }
+                _temperature = value;
+            }
+        }
+
+        public decimal Fahrenheit
+        {
+            get { return _temperature * 9 / 5 + 32; }
+        }
+
+        public decimal Kelvin
+        {
+            get { return _temperature + 273.15m; }
+        }
+
+        public decimal CelsiusToFahrenheit(decimal value)
+        { 
+            Celsius = value;
+            return Fahrenheit;
+        }
+
+        public decimal CelsiusToKelvin(decimal value)
+        {
+            Celsius = value;
+            return Kelvin;
+        }
+
+        public decimal FahrenheitToKelvin(decimal value)
+        {
+            Celsius = (value - 32) * 5 / 9;
+            return Kelvin;
+        }
+
+        public decimal FahrenheitToCelsius(decimal value)
+        {
+            Celsius = (value - 32) * 5 / 9;
+            return Celsius;
+        }
+
+        public decimal KelvinToFahrenheit(decimal value)
+        {
+            Celsius = value - 273.15m;
+            return Fahrenheit;
+        }
+
+        public decimal KelvinToCelsius(decimal value)
+        {
+            Celsius = value - 273.15m;
+            return Celsius;
+        }
+    }
+}
--- 006_Reflection/006_Reflection_HW/03_TemperatureConverter/Program.cs
+++ 006_Reflection/006_Reflection_HW/03_TemperatureConverter/Program.cs
@@ -4,12 +4,48 @@
  * При виконанні завдання використовуйте лише рефлексію. 
  */
 
+using System.Reflection;
+
 namespace TemperatureConverter
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Assembly ?assembly = null;
+
+            try
+            {
+                // Load the assembly from file
+                assembly = Assembly.LoadFrom("02_TemperatureConverterLibrary.dll");
+                Console.WriteLine("Loaded TemperatureConverterLibrary assembly");
+
+                // Get the type of the class
+                Type? converterType = assembly.GetType("TemperatureConverterLibrary.Temperature");
+                if (converterType == null)
+                {
+                    Console.WriteLine("Could not find the 'Temperature' type");
+                    return;
+                }
+
+                // Create an instance of the class
+                object? converterInstance = Activator.CreateInstance(converterType);
+
+                // Get the method for calling
+                MethodInfo? method = converterType.GetMethod("CelsiusToFahrenheit");
+
+                // Call the method with parameter
+                object[] parameters = { 20.5m };
+                object? result = method.Invoke(converterInstance, parameters);
+
+                Console.WriteLine($"20,5 Celsius is {result:0.##} Fahrenheit");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
             // Delay
             Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey();
