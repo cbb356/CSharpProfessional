79a0156a3ec1197fc593db727b2a03dc48808816
cbb356 <cbb356@gmail.com>
2025-09-26 22:27:03 +0300

    007_02_HW added

---
+++ 007_Attributes/007_Attributes_HW/02_Obsolete/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Створіть клас і застосуйте до його методів атрибут Obsolete спочатку у формі, 
+ * що просто виводить попередження, а потім у формі, що перешкоджає компіляції. 
+ * Продемонструйте роботу атрибута з прикладу виклику даних методів.
+ */
+
+namespace Obsolete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var obsoleteMethods = new ObsoleteMethods();
+            obsoleteMethods.OldMethod();
+            //obsoleteMethods.VeryOldMethod();    // Compilation error
+
+            // Delay
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+
+    internal class ObsoleteMethods
+    {
+        [Obsolete("This method is deprecated but you can use it")]
+        public void OldMethod()
+        {
+            Console.WriteLine("OldMethod calling");
+        }
+
+        [Obsolete("This method is deprecated and you can't use it", true)]
+        public void VeryOldMethod()
+        {
+            Console.WriteLine("VeryOldMethod calling");
+        }
+    }
+}
---
+++ 007_Attributes/007_Attributes_HW/05_AccessLevelAttribute/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Створіть атрибут користувача AccessLevelAttribute, який дозволяє визначити рівень доступу 
+ * користувача до системи. Сформуйте склад співробітників певної фірми як набору класів, 
+ * наприклад, Manager, Programmer, Director. За допомогою атрибута AccessLevelAttribute 
+ * розподіліть рівні доступу персоналу та відобразіть на екрані реакцію системи на спробу 
+ * кожного співробітника отримати доступ до захищеної секції.
+ */
+
+namespace AccessLevelAttribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Delay
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
