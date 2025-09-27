00a3505fa7c947b4829197c16aead9cf292527bd
cbb356 <cbb356@gmail.com>
2025-09-28 00:32:48 +0300

    008_02_HW added

---
+++ 008_Serialization/008_Serialization_HW/02_Serialization/PersonAttributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    [XmlType("Person")]
+    public class PersonAttributes
+    {
+        [XmlAttribute]
+        public string Name { get; set; }
+        [XmlAttribute]
+        public int Age { get; set; }
+    }
+}
--- 008_Serialization/008_Serialization_HW/02_Serialization/Program.cs
+++ 008_Serialization/008_Serialization_HW/02_Serialization/Program.cs
@@ -4,12 +4,34 @@
  * значення полів зберігалось в вигляді атрибутів елементів XML. 
  */
 
+using System.Xml.Serialization;
+
 namespace Serialization
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Serialize Person with Elements
+            PersonElements personElements = new PersonElements { Name = "John Doe", Age = 20};
+            using (FileStream stream = new FileStream("PersonElements.xml", FileMode.Create))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(PersonElements));
+                xmlSerializer.Serialize(stream, personElements);
+            }
+
+            Console.WriteLine($"{personElements} serialization completed");
+
+            // Serialize Person with Attributes
+            PersonAttributes personAttributes = new PersonAttributes() { Name = "Mary Shelly", Age = 21};
+            using (FileStream stream = new FileStream("PersonAttributes.xml", FileMode.Create))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(PersonAttributes));
+                xmlSerializer.Serialize(stream, personAttributes);
+            }
+
+            Console.WriteLine($"{personAttributes} serialization completed");
+
             // Delay
             Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey();
