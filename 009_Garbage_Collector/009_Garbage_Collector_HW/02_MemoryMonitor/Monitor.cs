a60205ed077e2dfae2d2b6d22c715557b13e59fc
cbb356 <cbb356@gmail.com>
2025-10-07 18:56:06 +0300

    009_02_HW added

---
+++ 009_Garbage_Collector/009_Garbage_Collector_HW/02_MemoryMonitor/Monitor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryMonitor
+{
+    internal class Monitor
+    {
+        private readonly long _maxMemoryBytes;
+        private readonly double _warningThreshold; // percentage (0–1.0)
+
+        public Monitor(long maxMemoryMB = 100, double warningTreshold = 0.8) 
+        {
+            _maxMemoryBytes = maxMemoryMB * 1024 * 1024;
+            _warningThreshold = warningTreshold;
+        }
+
+        // Return current memory usage
+        public double GetCurrentMemoryUsageMB()
+        {
+            return GC.GetTotalMemory(false) / (1024.0 * 1024.0);
+        }
+
+        // Check memory usage
+        public void CheckMemoryUsage()
+        {
+            double memoryUsedMB = GetCurrentMemoryUsageMB();
+            double memoryMaxMB = _maxMemoryBytes / (1024.0 * 1024.0);
+
+            Console.WriteLine($"Memory used: {memoryUsedMB:F2} MB / {memoryMaxMB:F2} MB");
+
+            if (memoryUsedMB >= memoryMaxMB)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Memory limit exceeded!");
+                Console.ResetColor();
+            }
+            else if (memoryUsedMB >= memoryMaxMB * _warningThreshold)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Memory usage close to limit");
+                Console.ResetColor();
+            }
+        }
+    }
+}
--- 009_Garbage_Collector/009_Garbage_Collector_HW/02_MemoryMonitor/Program.cs
+++ 009_Garbage_Collector/009_Garbage_Collector_HW/02_MemoryMonitor/Program.cs
@@ -11,6 +11,39 @@ namespace MemoryMonitor
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter maximum allowed memory in MB (positive number bigger than 0):");
+            if (!int.TryParse(Console.ReadLine(), out int memoryMax) || memoryMax <= 0)
+            {
+                Console.WriteLine("Wrong input for maximum memory");
+                return;
+            }
+
+            Console.WriteLine("Enter warning threshold in percents (1 - 100):");
+            if (!int.TryParse(Console.ReadLine(), out int warningThreshold) || (warningThreshold < 1 || warningThreshold > 100))
+            {
+                Console.WriteLine("Wrong input for warning threshold");
+                return;
+            }
+
+            Monitor monitor = new Monitor(memoryMax, warningThreshold / 100);
+
+            // Simulate memory workloa
+            var memoryConsumer = new List<byte[]>();
+
+            try
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    memoryConsumer.Add(new byte[1024 * 1024 * 10]);
+                    Thread.Sleep(500);
+                    monitor.CheckMemoryUsage();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
             // Delay
             Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey();
