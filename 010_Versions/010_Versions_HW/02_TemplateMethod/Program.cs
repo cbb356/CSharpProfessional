0d2120e31afd522e6b40bcbec1291a264a25f85d
cbb356 <cbb356@gmail.com>
2025-10-08 00:21:52 +0300

    010_HW created

---
+++ 010_Versions/010_Versions_HW/02_TemplateMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Вивчіть опис шаблону Template method (Шаблонний метод). Зверніть увагу на застосування шаблону, 
+ * а також на склад його учасників і зв'язку відносини між ними. Напишіть невелику програму мовою C#, 
+ * що є абстрактною реалізацією даного шаблону. 
+ */
+
+namespace TemplateMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Delay
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 010_Versions/010_Versions_HW/04_NonVirtualInterface/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Реалізуйте шаблон NVI у власній ієрархії успадкування.
+ */
+
+namespace NonVirtualInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Delay
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
