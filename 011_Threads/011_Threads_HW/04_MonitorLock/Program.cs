e78ae3d8630af50bccea1c385c949c678b7469d5
cbb356 <cbb356@gmail.com>
2025-10-11 10:04:55 +0300

    011_04_HW added

---
+++ 011_Threads/011_Threads_HW/04_MonitorLock/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Використовуючи конструкції блокування, модифікуйте останній приклад уроку таким чином, 
+ * щоб отримати можливість послідовної роботи 3-х потоків. 
+ */
+
+namespace MonitorLock
+{
+    class Program
+    {
+        static int counter = 0;
+        static object block = new object();
+
+        static void Function()
+        {
+            Monitor.Enter(block);
+            
+            Console.WriteLine($"\nThread {Thread.CurrentThread.GetHashCode()} started");
+            
+            for (int i = 0; i < 50; ++i)
+            {
+                Console.WriteLine($"Thread {Thread.CurrentThread.GetHashCode()} is working. Counter: {++counter}");
+                Thread.Sleep(10);
+            }
+            
+            Console.WriteLine($"Thread {Thread.CurrentThread.GetHashCode()} finished");
+            
+            Monitor.Exit(block);
+        }
+
+        static void Main()
+        {
+            Console.WriteLine($"Main thread started");
+
+            Thread[] threads = { new Thread(Function), new Thread(Function), new Thread(Function) };
+
+            // Start all threads
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+
+            // Wait for finishing all threads
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine($"\nMain thread finished");
+
+            // Delay
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
