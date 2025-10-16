62b97862bcc1899678bd169510bec9981b4132c1
cbb356 <cbb356@gmail.com>
2025-10-17 01:14:08 +0300

    012_03_HW added

--- 012_Core_Sync/012_Core_Sync_HW/03_NamedMutex/Program.cs
+++ 012_Core_Sync/012_Core_Sync_HW/03_NamedMutex/Program.cs
@@ -8,9 +8,28 @@ namespace NamedMutex
     {
         static void Main(string[] args)
         {
-            // Delay
-            Console.WriteLine("\nPress any key to continue...");
-            Console.ReadKey();
+            using (Mutex mutex = new Mutex(false, "MyMutex"))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Another program instance already running");
+                    Console.ResetColor();
+                    Console.WriteLine("\nPress any key to exit...");
+                    Console.ReadKey();
+                    return;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Only one program instance running");
+                Console.ResetColor();
+
+                // Delay    
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey();
+
+                mutex.ReleaseMutex();
+            }
         }
     }
 }
