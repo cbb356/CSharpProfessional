7e353e239d10996d724d9bda0d14621c61a9d92a
cbb356 <cbb356@gmail.com>
2025-10-17 09:21:28 +0300

    013_HW templates added

---
+++ 013_TPL/013_TPL_HW/02_ParallelMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Створіть два методи, які виконуватимуться у межах паралельних завдань. 
+ * Організуйте виклик цих методів за допомогою Invoke таким чином, 
+ * щоб основний потік програми (метод Main) не зупинив виконання.
+ */
+
+namespace ParallelMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Delay
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 013_TPL/013_TPL_HW/04_PLINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Створіть масив чисел розмірністю 1000000 або більше. Використовуючи генератор випадкових чисел, 
+ * проініціалізуйте цей масив значеннями. 
+ * Створіть PLINQ запит, який дозволить отримати усі непарні числа з вихідного масиву.
+ */
+
+namespace PLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Delay
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
