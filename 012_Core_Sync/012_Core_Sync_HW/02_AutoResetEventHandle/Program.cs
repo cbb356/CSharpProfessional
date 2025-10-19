c42d1112652572a84561c4e268d8049876a3b48d
cbb356 <cbb356@gmail.com>
2025-10-20 00:57:18 +0300

    012_02_HW added

--- 012_Core_Sync/012_Core_Sync_HW/02_AutoResetEventHandle/Program.cs
+++ 012_Core_Sync/012_Core_Sync_HW/02_AutoResetEventHandle/Program.cs
@@ -3,15 +3,40 @@
  * щоб замість ручної обробки використовувалася автоматична.
  */
 
+using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace AutoResetEventHandle
 {
     internal class Program
     {
+        static EventWaitHandle handle = null;
+
         static void Main(string[] args)
         {
-            // Delay
-            Console.WriteLine("\nPress any key to continue...");
-            Console.ReadKey();
+            handle = new EventWaitHandle(false, EventResetMode.AutoReset, "GlobalEvent::GUID");
+
+            Thread thread = new Thread(Function) { IsBackground = true };
+            thread.Start();
+
+            Console.WriteLine("\nPress any key to send signal");
+            while (true)
+            {
+                Console.ReadKey();
+                handle.Set();
+                Console.WriteLine("The signal has been sent. Press any key to send another one");
+            }
+        }
+
+        static void Function()
+        {
+            handle.WaitOne();
+
+            while (true)
+            {
+                Console.WriteLine("Hello world!");
+                Thread.Sleep(300);
+            }
         }
     }
 }
