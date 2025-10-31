653f59b3491b013f59c20e51c6e9dca06451845d
cbb356 <cbb356@gmail.com>
2025-10-31 13:14:32 +0200

    014_04_CW added

---
+++ 014_Async_Await/014_AsynCAwait_CW/04_AsyncAwaitLock/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Переробіть додаткове завдання з уроку 11 із використанням конструкції async await.
+ */
+
+namespace AsyncAwaitLock
+{
+    internal class Program
+    {
+        static int counter = 0;
+        static object fileLock = new object();
+
+        static void FunctionAsync(int taskId)
+        {
+            lock (fileLock)
+            {
+                Console.WriteLine($"\nTask {taskId} started");
+                for (int i = 0; i < 50; ++i)
+                {
+                    Console.WriteLine($"Task {taskId} is working. Counter: {++counter}");
+                    Thread.Sleep(10);
+                }
+                Console.WriteLine($"Task {taskId} finished");
+            }
+        }
+
+        static async Task Main()
+        {
+            Console.WriteLine($"Main started");
+
+            Task[] tasks = {
+                new Task(() => FunctionAsync(1)),
+                new Task(() => FunctionAsync(2)),
+                new Task(() => FunctionAsync(3))
+            };
+
+            foreach (var task in tasks)
+            {
+                task.Start();
+            }
+
+            await Task.WhenAll(tasks);
+
+            Console.WriteLine($"\nMain finished");
+
+            // Delay
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
