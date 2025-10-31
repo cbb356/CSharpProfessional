860e18f93696ae1e23ae7c20420cb5b4c4d3bfec
cbb356 <cbb356@gmail.com>
2025-10-31 10:50:24 +0200

    014_04_HW added

---
+++ 014_Async_Await/014_AsynCAwait_CW/01_Await/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+
+namespace _01_Await
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine($"Main started in {Thread.CurrentThread.ManagedThreadId}");
+            var operationTask = OperationAsync();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Main {i}");
+                Thread.Sleep(300);
+            }
+            int result = await operationTask;
+            Console.WriteLine(value: $"Result: {result}");
+            Console.WriteLine($"Main finished in {Thread.CurrentThread.ManagedThreadId}");
+
+            // Delay
+            Console.ReadKey();
+        }
+
+        public static async Task<int> OperationAsync()
+        {
+            Console.WriteLine($"OperationAsync started in {Thread.CurrentThread.ManagedThreadId}");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"OperationAsync {i}");
+                Thread.Sleep(500);
+            }
+            int result = await Task.Run(Operation);
+            Console.WriteLine($"OperationAsync finished in {Thread.CurrentThread.ManagedThreadId}");
+            return result;
+        }
+
+        public static int Operation()
+        {
+            Console.WriteLine($"Operation started in {Thread.CurrentThread.ManagedThreadId}");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Operation {i}");
+                Thread.Sleep(400);
+            }
+            Console.WriteLine($"Operation finished in {Thread.CurrentThread.ManagedThreadId}");
+            return 2;
+        }
+    }
+}
---
+++ 014_Async_Await/014_AsyncAwait_HW/04_AsyncAwaitLock/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Переробіть додаткове завдання з уроку 11 із використанням конструкції async await.
+ */
+
+namespace AsyncAwaitLock
+{
+    internal class Program
+    {
+        static int counter = 0;
+        static readonly SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
+
+        static async Task FunctionAsync(int taskId)
+        {
+            await semaphore.WaitAsync();
+
+            try
+            {
+                Console.WriteLine($"\nTask {taskId} started");
+                for (int i = 0; i < 50; ++i)
+                {
+                    Console.WriteLine($"Task {taskId} is working. Counter: {++counter}");
+                    await Task.Delay(10);
+                }
+                Console.WriteLine($"Task {taskId} finished");
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+
+        }
+
+        static async Task Main()
+        {
+            Console.WriteLine($"Main started");
+
+            Task[] tasks = { FunctionAsync(1), FunctionAsync(2), FunctionAsync(3) };
+
+            await Task.WhenAll(tasks);
+
+            Console.WriteLine($"\nMain finished");
+
+            // Delay
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
